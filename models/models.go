a28eae27d8b3817b0a61509328c6134376c368d3
Kolade-Amire <stephamire@gmail.com>
2025-02-16 09:47:10 +0100

    changed uuid id generation

--- models/models.go
+++ models/models.go
@@ -6,18 +6,30 @@ import (
 	"time"
 )
 
+type Id struct {
+	Id uuid.UUID `json:"id" gorm:"type:text;primaryKey"`
+}
+
+// BeforeCreate Hook for UUID Generation
+func (id *Id) BeforeCreate(db *gorm.DB) (err error) {
+	if id.Id == uuid.Nil {
+		id.Id = uuid.New()
+	}
+	return
+}
+
 type Artist struct {
 	gorm.Model
-	Id      uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
-	Name    string    `json:"name"`
-	Age     int       `json:"age"`
-	Country string    `json:"country"`
-	Albums  []Album   `gorm:"foreignKey:ArtistId"`
+	Id
+	Name    string  `json:"name"`
+	Age     int     `json:"age"`
+	Country string  `json:"country"`
+	Albums  []Album `gorm:"foreignKey:ArtistId"`
 }
 
 type Album struct {
 	gorm.Model
-	Id             uuid.UUID `json:"id" gorm:"type:uuid;primaryKey"`
+	Id
 	Name           string    `json:"name"`
 	ArtistId       uuid.UUID `json:"artistId" gorm:"type:uuid"`
 	NumberOfTracks int       `json:"numberOfTracks"`
